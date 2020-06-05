9868b198303ba726a7bc817d09398d56be7fb55e
bobby <devils6889@gmail.com>
2020-06-05 19:33:38 -0400

    remove redis

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ var logger = require('morgan');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
-var redis = require("redis");
-var redisClient = redis.createClient();
 
 var app = module.exports = express();
 var server = require('http').createServer(app);
